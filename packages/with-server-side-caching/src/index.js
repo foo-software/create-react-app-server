02d6db80d9610dd080c4f27edcab11d41b5886a7
Adam Henson <adamhenson1979@gmail.com>
2019-10-20 13:28:42 -0400

    chore: update tests.

--- packages/with-server-side-caching/src/index.js
+++ packages/with-server-side-caching/src/index.js
@@ -1,3 +1,3 @@
 export { default as CreateReactAppServerContext } from './CreateReactAppServerContext';
-export { default as withCreateReactAppServer } from './withCreateReactAppServer';
-export { default as withCreateReactAppServerProvider } from './withCreateReactAppServerProvider';
+export { default as withServerSideCaching } from './withServerSideCaching';
+export { default as withServerSideCachingProvider } from './withServerSideCachingProvider';
